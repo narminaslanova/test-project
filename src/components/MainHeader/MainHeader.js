899e8828265002d645ef3b77c684a15b63f386e8
narminaslanova <narminra@code.edu.az>
2022-01-21 18:09:53 +0200

    fixed

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
-      <h1>Test Page</h1>
+      <h1>Test Page (changed)</h1>
       <Navigation
         isLoggedIn={props.isAuthenticated}
         onLogout={props.onLogout}
