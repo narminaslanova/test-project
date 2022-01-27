e7666d4d3fbccb6f508cc54c9ad269d7c989a63d
narminaslanova <narminra@code.edu.az>
2022-01-27 13:43:29 +0200

    test commit

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes["main-header"]}>
-      <h1>Test Page (changed)</h1>
+      <h1>Test Page</h1>
       <Navigation />
     </header>
   );
